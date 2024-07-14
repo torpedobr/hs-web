9447c4b47a86dc3f0455af17909a66d12b51509e
Dmitry Konovalov <konoval87@gmail.com>
2024-07-14 13:48:09 +0200

    change templ

--- hsweb/chat/views.py
+++ hsweb/chat/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.http import HttpResponse
 from openai import OpenAI
+
+model_id = 'gpt-3.5-turbo-0125'
 client = OpenAI(
-    api_key="",
+    # defaults to os.environ.get("OPENAI_API_KEY")
+    api_key="sk-proj-Giv98bXOCyGnEyxzU5KyT3BlbkFJHYaLiXhfnVRpsHwiYw0o",
 )
 
 def chat(request):
@@ -17,21 +20,14 @@ def chat(request):
 
     return render(request, 'chat/chat.html')
 
-
-def ChatGPT_conversation(conversation):
-    client = OpenAI(api_key="sk-proj-JubBg4WHxHBezI1vx0icT3BlbkFJqkhAj0VB6774mnkujuHE")
-    model_id = 'gpt-3.5-turbo-0125'
-    response = client.chat.completions.create(
-        model=model_id,
-        messages=conversation
-    )
-    conversation.append({'role': response.choices[0].message.role, 'content': response.choices[0].message.content})
-    return conversation
 def ask_ai(text_data):
-    conversation = []
-    conversation.append({'role': 'system', 'content': 'How may I help you?'})
-    conversation = ChatGPT_conversation(conversation)
-
-    conversation.append({'role': 'user', 'content': text_data})
-    conversation = ChatGPT_conversation(conversation)
-    return('{0}: {1}\n'.format(conversation[-1]['role'].strip(), conversation[-1]['content'].strip()))
+    completion = client.chat.completions.create(
+      model=model_id,
+      messages=[
+        {"role": "system", "content": "Ты стервозная девочка которая покалывает парней пишущих ей. Иногда задавая вопросы."},
+        {"role": "user", "content": text_data}
+      ]
+    )
+    role = (completion.choices[0].message.role)
+    answer = (completion.choices[0].message.content)
+    return (role,":", answer )
--- hsweb/hsweb/settings.py
+++ hsweb/hsweb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'h8zp1_(qh$)pj)u29kpe#ky90d%usz#@c6q7=d+n6fn_#zqxqq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
